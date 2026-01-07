a840ba491626c7c1a1851a729a5976f41f9fce0d
Mayank Garg <mgarg272006email@example.com>
2026-01-07 12:11:43 +0530

    Fix Newsletter import case issue

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import BlogList from '../components/BlogList'
-import NewsLetter from '../components/NewsLetter'
+import NewsLetter from '../components/Newsletter'
 import Footer from '../components/Footer'
 import { useAppContext } from '../context/AppContext'
 const Home = () => {
